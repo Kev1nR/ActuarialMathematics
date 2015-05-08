38f6162a84df762765d2a78b3909845541bf0858
Kev1nR <kevinroche@blueyonder.co.uk>
2015-05-08 15:22:36 +0100

    Adding plot hovrer tooltips

--- ActuarialMathematics.WebUI/scripts/Directives/Charting.ts
+++ ActuarialMathematics.WebUI/scripts/Directives/Charting.ts
@@ -18,6 +18,24 @@
                 element: ng.IAugmentedJQuery,
                 attributes: IChartingAttributes) => {
 
+                function plothover(event, pos, item) {
+                    if (item) {
+                        var x = item.datapoint[0].toFixed(2),
+                            y = item.datapoint[1].toFixed(2);
+
+                        var tooltipText =
+                            item.series.label + ":<br/>"
+                            + attributes.xaxisTitle + " = " + x + "<br/>"
+                            + attributes.yaxisTitle + " = " + y;
+
+                        $("#tooltip").html(tooltipText)
+                            .css({ top: item.pageY + 5, left: item.pageX + 5 })
+                            .fadeIn(200);
+                    } else {
+                        $("#tooltip").hide();
+                    }
+                }
+
                 var options = {
                     series: {
                         lines: { show: true },
@@ -42,7 +60,7 @@
                         axisLabelFontFamily: 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
                         axisLabelPadding: 5
                     },
-                    grid: { hoverable: true },
+                    grid: { hoverable: true, clickable: true },
                     legend: { labelBoxBorderColor: "none", position: "left" }
                 };
 
@@ -51,6 +69,17 @@
                         $.plot($("#chart-placeholder"), newValue, options);
                     }
                 }, true);
+
+                $("<div id='tooltip'></div>").css({
+                    position: "absolute",
+                    display: "none",
+                    border: "1px solid #fdd",
+                    padding: "2px",
+                    "background-color": "#fee",
+                    opacity: 0.80
+                }).appendTo("body");
+
+                $("#chart-placeholder").on("plothover", this, plothover);                
             }
 
             return {
@@ -62,3 +91,4 @@
         }
     }
 }
+
